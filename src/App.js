9f0651f5bfda818862c259d13350b62bffed6a3c
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-03-30 20:30:43 -0700

    Books in the BookList component are now from the API

--- src/App.js
+++ src/App.js
@@ -22,7 +22,11 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className='app'>
-        <Route exact path='/' component={BookList} />
+        <Route
+          exact
+          path='/'
+          render={() => <BookList books={this.state.books} />}
+        />
         <Route exact path='/search' component={Search} />
       </div>
     );
