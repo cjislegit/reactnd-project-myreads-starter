5d109950810ea8b6092b164c01800fa781a70442
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-04-06 20:39:52 -0700

    Added comments to code

--- src/App.js
+++ src/App.js
@@ -12,16 +12,22 @@ class BooksApp extends React.Component {
   };
 
   onChange = (e, id) => {
+    //Finds the index of the book being updated by comparing IDs
     let index = this.state.books.findIndex((e) => e.id === id);
+    //Copies the state to newBooks
     let newBooks = this.state.books;
+    //Updates the shelf on the book with the matching ID
     newBooks[index].shelf = e;
+    //Updates the state
     this.setState({
       books: newBooks,
     });
+    //Updates the API
     BooksAPI.update({ id: id }, e);
   };
 
   componentDidMount() {
+    //Gets the books from the API and saves them to state
     BooksAPI.getAll().then((books) => {
       this.setState(() => ({
         books,
@@ -30,12 +36,17 @@ class BooksApp extends React.Component {
   }
 
   addBook = (e, newBook) => {
+    //Updates the shelf key with new value
     newBook.shelf = e;
+    //Copies state to newBooks
     let newBooks = this.state.books;
+    //Adds the new book to newBooks array
     newBooks.push(newBook);
+    //Updates state
     this.setState({
       books: newBooks,
     });
+    //Updates API with new book
     BooksAPI.update(newBook, e);
   };
 
